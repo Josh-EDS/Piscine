925018eecbb8e08a5e3787b4bb3343668f458d4b
josh-EDS <joshua.germany@epitech.digital>
2024-09-30 19:53:31 +0200

    Upload of the fourth exercise

---
+++ exercice4.js
@@ -0,0 +1,9 @@
+const b = true
+const c = 1
+const d = 42n 
+const e = Symbol()
+const f = new String("Bonjour")
+const g = function () {}
+// Reprise des exemples de l'exo 2
+
+document.getElementById("out").innerHTML = typeof a+"<br>"+typeof b+"<br>"+typeof c+"<br>"+typeof d+"<br>"+typeof e+"<br>"+typeof f+"<br>"+typeof g+"<br>"
