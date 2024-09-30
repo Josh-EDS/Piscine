58ee0f646db625c161f7cb4c50bffce27ea0f60e
josh-EDS <joshua.germany@epitech.digital>
2024-09-30 19:53:08 +0200

    Upload of the second exercise

---
+++ exercice2.js
@@ -0,0 +1,7 @@
+const bool = true
+const string = "forty two"
+const integer = 42
+const this_is_null = null
+const float = 42.42
+
+document.getElementById("out").innerHTML = "Bool = "+bool+"<br>String = "+string+"<br>Integer = "+integer+"<br>This_is_null = "+this_is_null+"<br>Float = "+float
