7513ba1bf25eb032115c524ddfd5ab15f1a695ac
josh-EDS <joshua.germany@epitech.digital>
2024-09-30 19:52:20 +0200

    Upload of the first exercise

---
+++ exercice1.js
@@ -0,0 +1,4 @@
+const greeting = 'Hello Wolrd'
+document.getElementById("out").innerHTML = greeting;
+
+/* document.getElementById("out").innerHTML modifie directement le texte en utilisant son ID. La fonction revient à faire la même chose, cependant, ce script s'exécutera peu importe la page HTML, ce qui ne serait pas le cas en utilisant la fonction. J'utiliserai dorénavant cette commande pour les futurs exercices. Cependant, si l'utilisation de cette commande est une compétence évaluée, elle est utilisée dans l'exercice 3. */
